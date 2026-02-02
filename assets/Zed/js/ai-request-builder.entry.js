6de1a780b22a50a05a8573d5b501f76aa5f2866e
Maksym Fedets <maksym.fedets@spryker.com>
2026-02-02 18:05:04 +0200

    Refactor AI content handling by implementing new structured response logic and enhancing type checks for the StructuredTransfer class.

---
+++ assets/Zed/js/ai-request-builder.entry.js
@@ -0,0 +1,188 @@
+export default class AiRequestBuilder {
+    constructor(
+        requestUrl = '',
+        requestMethod = 'POST',
+        requestBody = {},
+        targetPopoverSelector = 'data-target-popover',
+        sourceFieldSelector = 'data-source-field',
+        targetFieldSelector = 'data-target-field',
+        requestActionAttribute = 'data-request-action',
+        requestUrlAttribute = 'data-request-url',
+        requestTargetLocaleAttribute = 'data-request-target-locale',
+    ) {
+        this.requestUrl = requestUrl;
+        this.requestMethod = requestMethod;
+        this.requestBody = requestBody;
+        this.targetPopoverSelector = targetPopoverSelector;
+        this.sourceFieldSelector = sourceFieldSelector;
+        this.targetFieldSelector = targetFieldSelector;
+        this.requestActionAttribute = requestActionAttribute;
+        this.requestUrlAttribute = requestUrlAttribute;
+        this.requestTargetLocaleAttribute = requestTargetLocaleAttribute;
+        this.responseFieldSelector = '#response-field';
+        this.responseField = document.querySelector(this.responseFieldSelector) || null;
+        this.loadingPopover = document.querySelector('.loading-popover');
+        this.responsePopover = document.querySelector('.response-popover');
+        this.againBtn = document.querySelector('.js-ai-builder-again') || null;
+        this.applyBtn = document.querySelector('.js-ai-builder-apply') || null;
+        this.response = '';
+        this.sourceFields = [];
+        this.currentTargetFieldSelector = '';
+
+        this.init();
+    }
+
+    init() {
+        this.bindEvents();
+    }
+
+    bindEvents() {
+        // Start AI request
+        document.querySelectorAll('[data-ai-start]').forEach(button => {
+            button.addEventListener('click', () => {
+                this.requestBody = {};
+            })
+        })
+
+        // Set current locale
+        document.querySelectorAll('[data-current-locale]').forEach(button => {
+            button.addEventListener('click', (event) => {
+                this.requestBody.currentLocale = event.target.dataset.currentLocale || '';
+            })
+        })
+
+        // Show target popover
+        document.querySelectorAll(`[${this.targetPopoverSelector}]`).forEach(button => {
+            button.addEventListener('click', (event) => {
+                this.showTargetPopover(event);
+            })
+        })
+
+        // Set request action (translation, description improvement)
+        document.querySelectorAll(`[${this.requestActionAttribute}]`).forEach(button => {
+            button.addEventListener('click', (event) => {
+                this.requestBody.action = event.target.getAttribute(this.requestActionAttribute);
+                this.requestUrl = event.target.getAttribute(this.requestUrlAttribute);
+            })
+        })
+
+        // Set target locale
+        document.querySelectorAll(`[${this.requestTargetLocaleAttribute}]`).forEach(button => {
+            button.addEventListener('click', (event) => {
+                this.requestBody.locale = event.target.getAttribute(this.requestTargetLocaleAttribute);
+                this.currentTargetFieldSelector = this.currentTargetFieldSelector.replaceAll(this.requestBody.currentLocale, this.requestBody.locale);
+            })
+        })
+
+        document.querySelectorAll(`[${this.sourceFieldSelector}]`).forEach(button => {
+            button.addEventListener('click', (event) => {
+                const sources = event.target.dataset.sourceField.split(',').map(field => field.trim());
+                this.sourceFields = [];
+                sources.forEach(field => {
+                    this.sourceFields.push(document.querySelector(field));
+                })
+
+                this.requestBody.text = '';
+                this.sourceFields.forEach(field => {
+                    this.requestBody.text += (field.value || '');
+                })
+            })
+        })
+
+        document.querySelectorAll(`[${this.targetFieldSelector}]`).forEach(button => {
+            button.addEventListener('click', (event) => {
+                this.currentTargetFieldSelector = event.target.getAttribute(this.targetFieldSelector);
+            })
+        })
+
+        document.querySelectorAll('[data-request-ready]').forEach(button => {
+            button.addEventListener('click', (event) => {
+                if(this.requestBody.action === 'translation') {
+                    this.currentTargetFieldSelector = this.currentTargetFieldSelector.replaceAll(this.requestBody.currentLocale, this.requestBody.locale);
+                }
+                this.sendRequest();
+                console.log(this.requestBody);
+            })
+        })
+
+        document.querySelectorAll('[data-close-popover]').forEach(button => {
+            button.addEventListener('click', (event) => {
+                this.closePopovers();
+                this.requestBody = {};
+            })
+        })
+
+        this.againBtn.addEventListener('click', (event) => {
+            this.sendRequest();
+        })
+
+        this.applyBtn.addEventListener('click', (event) => {
+            document.querySelector(this.currentTargetFieldSelector).value = this.response;
+            this.closePopovers();
+        })
+    }
+
+    sendRequest() {
+        const formData = new FormData();
+        formData.append('text', this.requestBody.text);
+        formData.append('locale', this.requestBody.locale);
+
+        const requestOptions = {
+            method: this.requestMethod,
+            body: formData,
+        };
+        this.closePopovers();
+        this.toggleLoadingPopover();
+        // fetch(this.requestUrl, requestOptions)
+        //     .then(response => response.json())
+        //     .then(data => {
+        //         console.log('Success:', data);
+        //         this.toggleLoadingPopover(false);
+        //         this.response = this.responseField.value = data || '';
+        //         this.toggleResponsePopover();
+        //     })
+        //     .catch((error) => {
+        //         console.error('Error:', error);
+        //         this.toggleLoadingPopover(false);
+        //     });
+
+        const data = {
+            translation: 'kdjfh lkjhdlk gjhdfklj ghdflkjg',
+            improvedText: 'kdjfh lkjhdlk gjhdfklj ghdflkjg',
+        }
+
+        this.toggleLoadingPopover(false);
+        switch(this.requestBody.action) {
+            case 'translation':
+                this.response = this.responseField.value = data.translation || '';
+                break;
+            default:
+                this.response = this.responseField.value = data.improvedText || '';
+                break;
+        }
+        this.toggleResponsePopover();
+    }
+
+    toggleLoadingPopover(isVisible = true) {
+        isVisible ? this.loadingPopover?.showPopover() : this.loadingPopover?.hidePopover();
+    }
+
+    toggleResponsePopover(isVisible = true) {
+        isVisible ? this.responsePopover?.showPopover() : this.responsePopover?.hidePopover();
+    }
+
+    showTargetPopover(event) {
+        this.closePopovers();
+        document.querySelector(event.target.dataset.targetPopover)?.showPopover();
+    }
+
+    closePopovers() {
+        document.querySelectorAll('[popover]').forEach(popover => {
+            popover.hidePopover();
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    new AiRequestBuilder();
+})
